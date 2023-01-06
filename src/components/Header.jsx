b94952725907eec8831c76a607e3f65eba600149
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-01-05 20:43:36 -0400

    testing dependencies

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "../assets/Logo.png"
+// import Escudo from "../assets/escudo.svg"
+
+export const Header = () => {
+    return(
+        <div className="">
+            <div className="flex place-content-between"> 
+            <h1>Hola mama</h1>
+                {/* <img src={Logo}/> */}
+                {/* <img src={Escudo}/> */}
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <HashRouter>
+        <App/>        
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>
 )
